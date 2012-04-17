a3b2a83ae412dec7369c7e4dc94b0e4cdab9ac86
cai xiaoxiao <caixiaoxiao117@gmail.com>
2012-04-17 14:06:26 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorStudy::Application.config.secret_token = '252da9ae2cf38512b817392b8adff8c1ff90b7b0f18179954b926d76df07415303b762081eaa37a6bf27b6ca39e385b2c6561144215ea9ed3df58a7e49f8899c'
